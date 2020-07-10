055f9391f0246e9e7184894a772ed4e56f2e9e31
McJohnsonLin <48139258+LINMINGCHENG@users.noreply.github.com>
2020-07-10 18:51:02 -0400

    added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,71 @@
+const Users=require('../models/users')
+const AWS= require('aws-sdk')
+const jwt= require('jsonwebtoken')
+const { token } = require('morgan')
+
+
+
+AWS.config.update({
+   acessKeyId:process.env.AWS_ACCESS_KEY,
+   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+   region: process.env.AWS_REGION
+})
+
+const ses= new AWS.SES({apiVersion:'2010-12-01'})
+
+
+
 exports.register = (req, res)=>{
-   console.log('REGISTER CONTROLLER',req.body);
+ 
+   const {name,email,password} = req.body;
+
+    //check if users exist in our dbpath
+    Users.findOne({email}).exec((err,user) =>{
+       if(user){
+             return res.status(400),json({
+                err:'Email is token'
+             });
+       }
+
+       //generate token with user name email and password
+     const token =jwt.sign({name,email,password},process.env.JWT_ACCOUNT_ACTIVETION,{
+         expiresIn:'19m'
+      })
+     
+
+           //send email
+        const params = {
+           Source: process.env.EMAIL_FROM,
+           Destination: {
+              ToAddresses: [email]
+           },
+           ReplyToAddresses: [process.env.EMAIL_TO],
+           Message: {
+              Body: {
+                 Html: {
+                    Charset: "UTF-8",
+                    Data: `<html><body><h1>${name}<p>${process.env.CLIENT_URL}/auth/activate/${token}</p></h1></body></html>`
+                 }
+              },
+              Subject: {
+                 Charset: 'UTF-8',
+                 Data: "Complete your registerzation"
+              }
+         }};
+          const sendEmailOnRegister = ses.sendEmail(params).promise()
+
+
+        sendEmailOnRegister.
+        then((data) => {
+              console.log('email submiited to SES', data),
+                 res.send('email sent')
+           })
+           .catch((err) => {
+              console.log('ses email on register', err);
+              res.send('email failed')
+           })
+    });
+
 }
+
+
--- server/models/users.js
+++ server/models/users.js
@@ -1,48 +1,56 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 
-const crypto =  require('crypto');
-
-
-const userSchema = new mongoose.Schema(
-    {
-        username:{
-            type:String,
-            trim:true,
-            required:true,
-            max:12,
-            unique:true,
-            index:true,
-            lowercase:true
-        },
-        name:{
-            type:String,
-            trim:true,
-            required:true,
-            max:32
-        },
-        email:{
-            type:String,
-            trim:true,
-            required:true,
-            unique:true,
-            lowercase:true
-         },
-         hashed_password:{
-             type:String,
-             required:true,
-         },
-         salt:String,
-         role:{
-             type:String,
-             default:'subscriber'
-         },
-         resetPassordLink:{
-            data:String,
-            default:''
-         }
-    },{timestamps:true}
-)
-
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 12,
+        unique: true,
+        index: true,
+        lowercase: true
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    profile: {
+        type: String,
+        required: true
+    },
+    hashed_password: {
+        type: String,
+        required: true
+    },
+    salt: String,
+    about: {
+        type: String
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+    resetPasswordLink: {
+        data: String,
+        default: ''
+    }
+}, {
+    timestamps: true
+});
 
 //virtual fields
 
@@ -63,28 +71,26 @@ userSchema.virtual('password')
 
 
 //method  > authentication encryptPassword MakeSalt
-userSchema.methods ={
+userSchema.methods = {
+    authenticate: function (plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
 
-  authenticate: function(plainText){
-     return this.encryptPassword(plainText) === this.hashed_password;
-  },
+    encryptPassword: function (password) {
+        if (!password) return '';
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return '';
+        }
+    },
 
-  encryptPassword:function(password) {
-      if(!password) return ''
-      try {
-          return crypto.crypto.createHmac('sha1', this.salt)
-              .update(password)
-              .digest('hex');
-      } catch (error) {
-          return ''
-      }
-  },
-
-
-  makeSalt:function(){
-   return Math.round(new Date().valueOf() * Math.random()) + '';
-  }
-
+    makeSalt: function () {
+        return Math.round(new Date().valueOf() * Math.random()) + '';
+    }
 };
 
-module.exports = mongoose.model('User');
+module.exports = mongoose.model('User', userSchema);
